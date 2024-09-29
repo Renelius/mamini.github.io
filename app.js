9d27528fc9d526547957271ff3611306902c9313
ponchik009 <lolikonru@mail.ru>
2024-09-29 17:08:51 +0800

    merge main and cart page

--- app.js
+++ app.js
@@ -86,7 +86,8 @@ manageCountBlocks.forEach((block, i) => {
 });
 
 cartButton.addEventListener("click", () => {
-  Telegram.WebApp.openTelegramLink("cart.html");
+  console.log(JSON.stringify(order));
+  executeCart();
 });
 
 function countForCart(container) {
@@ -102,9 +103,116 @@ function saveOrderToLS() {
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
   console.log(JSON.stringify(order));
+  executeCart();
   // tg.sendData(JSON.stringify(order));
-  Telegram.WebApp.openTelegramLink("cart.html");
+  // Telegram.WebApp.openTelegramLink("cart.html");
 });
+
+function executeCart() {
+  let tg = window.Telegram.WebApp;
+
+  tg.expand();
+
+  tg.MainButton.textColor = "#FFFFFF";
+  tg.MainButton.color = "#2cab37";
+
+  tg.MainButton.show();
+
+  let order = JSON.parse(loadCartFromLS() || "{}");
+
+  const container = document.querySelector(".cart-container");
+  container.innerHTML = "";
+  Object.values(order || {}).forEach((dish) => {
+    const item = document.createElement("div");
+    item.classList.add("cart-item");
+
+    const itemImg = document.createElement("img");
+    itemImg.classList.add("cart-item-img");
+    itemImg.src = dish.img;
+    itemImg.alt = dish.name;
+
+    const itemMainBlock = document.createElement("div");
+    itemMainBlock.classList.add("cart-item-main");
+
+    const itemInfoBlock = document.createElement("div");
+    itemInfoBlock.classList.add("cart-item-info");
+    const itemName = document.createElement("span");
+    itemName.classList.add("cart-item-name");
+    itemName.innerText = dish.name;
+    const itemPrice = document.createElement("span");
+    itemPrice.innerText = dish.price * dish.count + " ₽";
+    itemPrice.classList.add("cart-item-price");
+    itemInfoBlock.append(itemName, itemPrice);
+
+    const itemActionsBlock = document.createElement("div");
+    itemActionsBlock.classList.add("cart-item-actions");
+    const dishCount = document.createElement("span");
+    dishCount.innerText = dish.count;
+    dishCount.classList.add("cart-item-count");
+    const buttonDec = document.createElement("button");
+    buttonDec.innerText = "-";
+    buttonDec.classList.add("button-dec");
+    buttonDec.addEventListener("click", () => {
+      const dishesCount = Math.max(0, (+dishCount.innerText || 0) - 1);
+
+      dishCount.innerText = dishesCount;
+      itemPrice.innerText = `${dishesCount * dish.price} ₽`;
+      if (dishesCount === 0) {
+        item.remove();
+      }
+
+      order[dish.id].count = dishesCount;
+
+      saveOrderToLS();
+      updateTgButtonText();
+
+      if (Object.values(order).every((dish) => dish.count === 0)) {
+        tg.MainButton.hide();
+      }
+    });
+
+    const buttonInc = document.createElement("button");
+    buttonInc.innerText = "+";
+    buttonInc.classList.add("button-inc");
+    buttonInc.addEventListener("click", () => {
+      const dishesCount = (+dishCount.innerText || 0) + 1;
+
+      dishCount.innerText = dishesCount;
+      itemPrice.innerText = `${dishesCount * dish.price} ₽`;
+
+      order[dish.id].count = dishesCount;
+
+      saveOrderToLS();
+      updateTgButtonText();
+    });
+    itemActionsBlock.append(buttonDec, dishCount, buttonInc);
+
+    itemMainBlock.append(itemInfoBlock, itemActionsBlock);
+
+    item.append(itemImg, itemMainBlock);
+
+    container.appendChild(item);
+    container.classList.add("cart-container_open");
+  });
+
+  function loadCartFromLS() {
+    return localStorage.getItem("restoraunt_order");
+  }
+
+  function updateTgButtonText() {
+    const totalPrice = Object.values(order || {}).reduce(
+      (acc, cur) => acc + cur.price * cur.count,
+      0
+    );
+    tg.MainButton.setText(`Заказать на сумму ${totalPrice} ₽`);
+  }
+
+  Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    console.log(JSON.stringify(order));
+    tg.sendData(JSON.stringify(order));
+    tg.close();
+  });
+}
 
 // let usercard = document.getElementById("usercard");
 
